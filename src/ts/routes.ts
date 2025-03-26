a770c69224a9f1d10dd321e1b44e7353a44fc1e7
cedy123 <cedrickgueta056@gmail.com>
2025-03-26 14:35:08 +0800

    asdasd

--- src/ts/routes.ts
+++ src/ts/routes.ts
@@ -1,18 +1,14 @@
 import HomePage from "../pages/home/home";
 
-import GamePage from "../pages/game";
-
 import DynamicRoutePage from "../pages/dynamic-route";
 import RequestAndLoad from "../pages/request-and-load";
 import NotFoundPage from "../pages/not-found/404";
-import LotteryPage from "../pages/Lottery";
+import LotteryPage from "../pages/lottery/Lottery";
 import SportsPage from "../pages/sports/sports";
 import VideoPage from "../pages/video/video";
 import ElectronicPage from "../pages/electronic/electronic";
 import ChessPage from "../pages/chess/Chess";
 import FishingPage from "../pages/fishing/fishing";
-import OffersPage from "../pages/offers";
-import InformationPage from "../pages/Information";
 
 const routes = [
   { path: "/", component: HomePage, options: { animate: false } },
@@ -47,17 +43,7 @@ const routes = [
     component: FishingPage,
     options: { animate: false },
   },
-  {
-    path: "/offers/",
-    component: OffersPage,
-    options: { animate: false },
-  },
 
-  {
-    path: "/information/",
-    component: InformationPage,
-    options: { animate: false },
-  },
   {
     path: "/dynamic-route/blog/:blogId/post/:postId/",
     component: DynamicRoutePage,
