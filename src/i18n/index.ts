40ba5f94a3bba2e7ebe76f030eed3466c479e80b
cedy123 <cedrickgueta056@gmail.com>
2025-03-26 17:09:02 +0800

    update i18

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,23 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import en from "../locales/en.json";
+import tl from "../locales/ph.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {
-      translation: {
-        title: "Welcome to my App",
-        description: "This is a multilingual app using i18n.",
-        not_found: "Oops! The page you're looking for doesn't exist.",
-        go_home: "Go Home",
-      },
+      translation: en,
     },
     tl: {
-      translation: {
-        title: "Maligayang pagdating sa aking App",
-        description: "Ito ay isang multilingual na app gamit ang i18n.",
-        not_found: "Paumanhin! Ang pahinang hinahanap mo ay hindi umiiral.",
-        go_home: "Bumalik sa Home",
-      },
+      translation: tl,
     },
   },
   lng: "en", // Default language
--- src/locales/en.js
+++
@@ -1,5 +0,0 @@
-// src/locales/en.js
-export default {
-  welcome: "Welcome to Framework7",
-  // other strings...
-};
