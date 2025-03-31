e04f45d89f7e17999e76e962e5468cc121ae5513
cedy123 <cedrickgueta056@gmail.com>
2025-03-31 11:51:32 +0800

    add mobile header

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import { Block, Page } from "framework7-react";
+import { Block, Icon, Link, Page } from "framework7-react";
 import { useTranslation } from "react-i18next";
 
 import { LoginModal } from "@/components/LoginModal";
@@ -13,6 +13,20 @@ const HomePage = () => {
   const { t } = useTranslation();
   return (
     <Page>
+      {/* Mobile Banner Layout */}
+      <div className="flex w-full justify-start bg-gradient-to-r from-blue-300 to-blue-600 p-5 md:hidden">
+        <Link tabLink="#view-home">
+          <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-xl font-bold text-transparent">
+            U8.COM
+          </span>
+          <span className="text-xs font-light">Chinese Gaming</span>
+        </Link>
+        <Link href="#">
+          <Icon f7="logo_telegram" className="text-blue-500" />
+          <span className="ml-1 text-xs">@t.u2support</span>
+        </Link>
+      </div>
+
       <Swiper
         slidesPerView={1}
         pagination={{ clickable: true }}
