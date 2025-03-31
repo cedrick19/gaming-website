bb42918a5a66b16a3d98c266a9f55a45ee8250e4
sushikansai <michaelsagun013@gmail.com>
2025-03-31 13:20:00 +0800

    wip: changes in CustomModal

--- src/components/CustomModal/CustomModal.tsx
+++ src/components/CustomModal/CustomModal.tsx
@@ -3,12 +3,18 @@ import { Block, Page, Popup } from "framework7-react";
 export const CustomModal = ({
   id,
   children,
+  className,
+  onOpen,
+  onClose,
 }: {
   id: string;
   children?: React.ReactNode;
+  className?: string;
+  onOpen?: () => void;
+  onClose?: () => void;
 }) => {
   return (
-    <Popup id={id}>
+    <Popup id={id} className={className} onPopupClose={onClose} onPopupOpen={onOpen}>
       <Page>
         <Block>{children}</Block>
       </Page>
