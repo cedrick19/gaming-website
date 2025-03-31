370542fc8c97c961230aac3083ed9e74dfaf35c8
cedy123 <cedrickgueta056@gmail.com>
2025-03-31 11:16:39 +0800

    add welcome modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
+import type React from "react";
+
 import { App, View } from "framework7-react";
+import { useState, useEffect } from "react";
 import routes from "./ts/routes";
 import NavBar from "./components/nav-bar/Navigation";
 import { AuthProvider } from "./components/AuthContext";
+import WelcomeModal from "./components/WelcomeModal/welcome-modal";
 
 const appConfig = {
   name: "gamingwebsite",
@@ -10,11 +14,23 @@ const appConfig = {
 };
 
 const MyApp: React.FC = () => {
+  const [modalOpened, setModalOpened] = useState(false);
+
+  useEffect(() => {
+    setModalOpened(true);
+  }, []);
+
+  const handleModalClose = () => {
+    setModalOpened(false);
+  };
+
   return (
     <AuthProvider>
       <App {...appConfig}>
         <NavBar />
         <View main tab url="/" browserHistory browserHistorySeparator="" />
+
+        <WelcomeModal opened={modalOpened} onModalClosed={handleModalClose} />
       </App>
     </AuthProvider>
   );
