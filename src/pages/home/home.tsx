1deb10f3ff24a065e944b33cdd1590951d2deebb
cedy123 <cedrickgueta056@gmail.com>
2025-04-03 11:08:51 +0800

    update swiper

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,12 @@
-import { Block, Icon, Link, Page, Navbar, NavLeft, Button } from "framework7-react";
+import {
+  Block,
+  Icon,
+  Link,
+  Page,
+  Navbar,
+  NavLeft,
+  Button,
+} from "framework7-react";
 import { useTranslation } from "react-i18next";
 
 import { LoginModal } from "@/components/LoginModal";
@@ -27,7 +35,11 @@ const HomePage = () => {
               <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-xl font-bold text-transparent">
                 U8.COM
               </span>
-              <span className={`text-xs font-light ${getDevice().ios && "text-black"}`}>Chinese Gaming</span>
+              <span
+                className={`text-xs font-light ${getDevice().ios && "text-black"}`}
+              >
+                Chinese Gaming
+              </span>
             </Link>
             <Link href="#">
               <Icon f7="logo_telegram" className="text-blue-500" />
@@ -52,35 +64,37 @@ const HomePage = () => {
             prevEl: ".swiper-button-prev",
           }}
           modules={[Navigation, Pagination, Autoplay]}
-          className="w-full"
+          className="h-52 w-full sm:h-10 md:h-auto"
         >
           <SwiperSlide>
             <img
               src="./assets/image/carousel1.jpg"
               alt="Promotional Banner 1"
-              className="h-auto w-full"
+              className="h-full w-full"
             />
           </SwiperSlide>
           <SwiperSlide>
             <img
               src="./assets/image/carousel2.jpg"
               alt="Promotional Banner 2"
-              className="h-auto w-full"
+              className="h-full w-full"
             />
           </SwiperSlide>
         </Swiper>
 
-        <Button
-          className={`swiper-button-prev absolute left-4 top-1/2 hidden bg-opacity-60 p-4 text-white transition-opacity duration-300 md:flex ${
-            isHovered ? "opacity-100" : "opacity-0"
-          }`}
-        />
+        <div className="hidden md:block">
+          <Button
+            className={`swiper-button-prev absolute left-4 top-1/2 hidden bg-opacity-60 p-4 text-white transition-opacity duration-300 md:flex ${
+              isHovered ? "opacity-100" : "opacity-0"
+            }`}
+          />
 
-        <Button
-          className={`swiper-button-next absolute right-4 top-1/2 hidden bg-opacity-60 p-4 text-white transition-opacity duration-300 md:flex ${
-            isHovered ? "opacity-100" : "opacity-0"
-          }`}
-        />
+          <Button
+            className={`swiper-button-next absolute right-4 top-1/2 hidden bg-opacity-60 p-4 text-white transition-opacity duration-300 md:flex ${
+              isHovered ? "opacity-100" : "opacity-0"
+            }`}
+          />
+        </div>
       </div>
 
       <Layout>
