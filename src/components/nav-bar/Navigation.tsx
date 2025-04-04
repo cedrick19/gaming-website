505a6e3328f789b5b68aa533f9c102a69068253d
sushikansai <michaelsagun013@gmail.com>
2025-04-04 18:49:31 +0800

    wip: changed NavBar into a single imported SVG file

--- src/components/nav-bar/Navigation.tsx
+++ src/components/nav-bar/Navigation.tsx
@@ -12,6 +12,7 @@ import {
 } from "framework7-react";
 import { useAuth } from "@/hooks/useAuth";
 import { getDevice } from "framework7";
+import NavBarBg from "@/assets/image/navbar-bg.svg"
 
 const NavLinks = ({
   isMobile,
@@ -186,19 +187,7 @@ const NavBar = () => {
           icons
           className="relative transition-all !bg-transparent !h-20"
         >
-          <svg
-            className="absolute bottom-0 left-0 z-0 w-full h-full shadow-secondary/30"
-            viewBox="0 0 440 85"
-            preserveAspectRatio="xMidYMid slice"
-          >
-            <path
-              d="M220 57.0004C239.882 57.0004 256 40.8827 256 21.0004C256 13.0706 262.105 3.94169 270 4.68079L436.118 20.2315C442.286 20.8089 447 25.9852 447 32.1793V71.0002C447 78.7322 440.732 85.0002 433 85.0002H7C-0.731989 85.0002 -7 78.7322 -7 71.0002V32.1793C-7 25.9852 -2.2856 20.8088 3.88154 20.2315L170 4.68079C177.895 3.94169 184 13.0706 184 21.0004C184 40.8827 200.118 57.0004 220 57.0004Z"
-              fill="white"
-              stroke="#4C2CCA"
-              strokeOpacity="0.3"
-              strokeWidth="0.7"
-            />
-          </svg>
+          <img src={NavBarBg} className="absolute bottom-0 left-0 z-0 w-full h-full object-cover"/>
           <NavLinks
             isMobile={isMobile}
             activeTab={activeTabId}
